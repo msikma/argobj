ba64f97f1bb0bd0797caa82d451b3057d46f9597
Michiel Sikma <michiel@sikma.org>
2019-06-16 16:01:13 +0200

    Change to a nicer example and add output example at the top

--- example.js
+++ example.js
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
 
+// Usage example of argObj.
+// Running './example --help' should print the following:
+
+// usage: example.js [-h] [-v] [--output TYPE] [-a NAME] [--query QUERY]
+//                   [--category CATEGORY]
+//
+// Suspendisse at sodales leo, in bibendum ex.
+//
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac lectus
+// lacinia, laoreet sem sit amet, imperdiet tellus. Etiam augue erat, elementum
+// vel malesuada non, varius quis lectus. Phasellus quis sollicitudin dui, nec
+// tristique ex.
+//
+// Optional arguments:
+//   -h, --help           Show this help message and exit.
+//   -v, --version        Show program's version number and exit.
+//   --output TYPE        Result output format.
+//      {json,              JSON string (default).
+//       xml,               XML string.
+//       terminal}          Plain text readable in terminal.
+//   -a, --author NAME    Author of the work.
+//
+// Search options:
+//   --query QUERY        Query string to search for.
+//   --category CATEGORY  Specific category ID.
+//
+// For more information, see <https://michielsikma.com/>.
+
 const makeArgObj = require('./src/index')
 const parser = makeArgObj({
   addHelp: true,
@@ -7,18 +35,43 @@ const parser = makeArgObj({
   longDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac lectus lacinia, laoreet sem sit amet, imperdiet tellus. Etiam augue erat, elementum vel malesuada non, varius quis lectus. Phasellus quis sollicitudin dui, nec tristique ex.`,
   noWrapping: false,
   ensurePeriod: false,
+  noDoubleMetavars: true,
   epilog: `For more information, see <https://michielsikma.com/>.`,
   version: '1.0.0'
 })
-
-parser.addArgument(['--action'], { help: 'Which action to take.', choices: ['search', 'detail'], metavar: 'ACTION', choicesHelp: ['Runs a search query and returns results.', 'Returns information about a specific item by ID.'] })
-parser.addArgument(['--output'], { help: 'Result output format.', choices: ['json', 'xml', 'terminal'], choicesHelp: ['JSON string (default).', 'XML string.', 'Plain text readable in terminal.'], defaultValue: 'json', metavar: 'TYPE' })
-parser.addArgument(['--site'], { help: 'Site to run the query on.', choices: ['asdf'], defaultValue: 'asdf' })
+parser.addArgument(
+  ['--output'],
+  {
+    help: 'Result output format.',
+    choices: ['json', 'xml', 'terminal'],
+    choicesHelp: ['JSON string (default).', 'XML string.', 'Plain text readable in terminal.'],
+    defaultValue: 'json',
+    metavar: 'TYPE'
+  }
+)
+parser.addArgument(
+  ['-a', '--author'],
+  {
+    help: 'Author of the work.',
+    metavar: 'NAME',
+    defaultValue: 'asdf'
+  }
+)
 
 // Search options:
 parser.addSection('Search options:')
-parser.addArgument(['--query'], { help: 'Query string to search for.' })
-parser.addArgument(['--category'], { help: 'Specific category ID.' })
+parser.addArgument(
+  ['--query'],
+  {
+    help: 'Query string to search for.'
+  }
+)
+parser.addArgument(
+  ['--category'],
+  {
+    help: 'Specific category ID.'
+  }
+)
 
 const parsed = parser.parseArgs()
 
